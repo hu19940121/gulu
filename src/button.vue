9b776bfe78da6ae63b2eef7c5eea1126a54b5de1
kaier <673822024@qq.com>
2019-12-27 01:34:46 +0800

    添加parcel构建工具

---
+++ src/button.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button class="g-button">hi</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss"  scoped>
+  .g-button{
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border:1px solid var(--border-color);
+    background-color: var(--button-bg);
+    &:hover{
+    border-color: var(--border-color-hover);
+    }
+    &:active{
+      background-color: var(--border-active-bg);
+    }
+    &:focus{
+      outline: none;
+    }
+  }
+</style>
